4b31bf7ad178fe8610b70d6241ad54befb0cec3f
arjayvictorio <41265544+arjayvictorio@users.noreply.github.com>
2022-09-17 10:28:14 +0800

    Delete ReadsMinimalAPI directory

--- ReadsMinimalAPI/Models/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ReadsMinimalAPI.Models
-{
-    public class Book
-    {
-        public int Id { get; set; } = 0;
-        public string Name { get; set; }=string.Empty;
-        public string Description { get; set; }=string.Empty;
-    }
-}
